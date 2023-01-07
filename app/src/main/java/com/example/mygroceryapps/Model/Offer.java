d3d34172ac1b490e33e62f51e94562f3380a8c29
Sumaya-1006 <111642545+Sumaya-1006@users.noreply.github.com>
2023-01-07 16:01:26 +0600

    Grocery Apps

---
+++ app/src/main/java/com/example/mygroceryapps/Model/Offer.java
@@ -0,0 +1,11 @@
+package com.example.mygroceryapps.Model;
+
+public class Offer {
+    public String describtion , img;
+
+    public Offer(String describtion, String img) {
+
+        this.describtion = describtion;
+        this.img = img;
+    }
+}
